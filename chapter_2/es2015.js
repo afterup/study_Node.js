81a3dd33d5bd1ffabe80fff5db5b01acd83a73bc
Sim Ah young <UPTOHAI@Simui-MacBook-Pro.local>
2019-10-15 18:09:05 +0900

    study chapter7,9-1

--- chapter_2/es2015.js
+++ chapter_2/es2015.js
@@ -41,10 +41,74 @@ function add1(x,y){
 const add2 = (x,y)=> (x+y);
 
 //비구조화 할당
+var getCandy = candyMachine.getCandy;
+var count = candyMachine.status.count;
+//->
+const { getCandy, status:{ count }} = candyMachine;
+//배열 비구조화
+var array = ['node',{},10,true];
+var node = array[0];
+//->
+const [node,obj,bool] = array;
 
 //프로미스
+const condition = true;
+const promise = new Promise((resolve,reject)=>{
+    if(condition){
+        resolve('성공');
+    }else{
+        reject('실패');
+    }
+});
+
+promise
+    .then((message)=>{
+        console.log(message); //resolve한 경우 실행
+    })
+    .catch((error)=>{
+        console.error(error);// reject한 경우 실행
+    });
+//
+const promise1 = Promise.resolve('성공');
+const promise2 = Promise.resolve('성공');
+Promise.all([promise1,promise2])
+    .then((result)=>{
+        console.log(result);
+    })
+    .catch((error)=>{
+        console.error(error);
+    });
 
 //async/await
+function findAndSaveUser(Users){
+    Users.findOne({})
+        .then((user)=>{
+            user.name = 'zero';
+            return user.save();
+        })
+        .then((user)=>{
+            return Users.findOne({gender:'m'});
+        })
+        .catch(err=>{
+            console.error(err);
+        });
+}
+
+async function findAndSaveUser(Users){
+    let user = await Users.findOne({});
+    user.name = 'zero';
+    user = await user.save();
+    user = await Users.findOne({gender:'m'});
+}
+//
+const promise1 = Promise.resolve('성공');
+const promise2 = Promise.resolve('성공');
+(async() => {
+    for await(promise of [promise1,promise2]){
+        console.log(promise);
+    }
+})();
+
 
 //AJAX
 
